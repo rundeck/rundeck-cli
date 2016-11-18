9ebf2c0b5c2f2db288cef37be4df96c712771d26
Greg Schueler <greg.schueler@gmail.com>
2016-11-17 16:24:01 -0800

    fix #3 add basic RD_FORMAT=yaml/json support

--- src/main/java/org/rundeck/client/tool/App.java
+++ src/main/java/org/rundeck/client/tool/App.java
@@ -4,11 +4,20 @@ import com.simplifyops.toolbelt.CommandRunFailure;
 import com.simplifyops.toolbelt.InputError;
 import com.simplifyops.toolbelt.Tool;
 import com.simplifyops.toolbelt.ToolBelt;
+import com.simplifyops.toolbelt.format.json.jackson.JsonFormatter;
+import com.simplifyops.toolbelt.format.yaml.snakeyaml.YamlFormatter;
 import com.simplifyops.toolbelt.input.jewelcli.JewelInput;
 import org.rundeck.client.Rundeck;
 import org.rundeck.client.api.RundeckApi;
+import org.rundeck.client.api.model.DateInfo;
+import org.rundeck.client.api.model.Execution;
+import org.rundeck.client.api.model.JobItem;
+import org.rundeck.client.api.model.ScheduledJobItem;
 import org.rundeck.client.tool.commands.*;
 import org.rundeck.client.util.Client;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.IOException;
 
@@ -18,10 +27,35 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args) throws IOException, CommandRunFailure {
-        tool("rd").runMain(args, true);
+        setup(tool("rd"), args).runMain(args, true);
     }
 
-    public static Tool tool(final String name) {
+    public static Tool setup(final ToolBelt belt, final String[] args) {
+        setupFormat(belt);
+        return belt.buckle();
+    }
+
+    private static void setupFormat(final ToolBelt belt) {
+        if ("yaml".equalsIgnoreCase(Env.getString("RD_FORMAT", null))) {
+            DumperOptions dumperOptions = new DumperOptions();
+            dumperOptions.setDefaultFlowStyle(
+                    "BLOCK".equalsIgnoreCase(Env.getString("RD_YAML_FLOW", "BLOCK")) ?
+                    DumperOptions.FlowStyle.BLOCK :
+                    DumperOptions.FlowStyle.FLOW
+            );
+            dumperOptions.setPrettyFlow(Env.getBool("RD_YAML_PRETTY", true));
+            Representer representer = new Representer();
+            representer.addClassTag(JobItem.class, Tag.MAP);
+            representer.addClassTag(ScheduledJobItem.class, Tag.MAP);
+            representer.addClassTag(DateInfo.class, Tag.MAP);
+            representer.addClassTag(Execution.class, Tag.MAP);
+            belt.formatter(new YamlFormatter(representer, dumperOptions));
+        } else if ("json".equalsIgnoreCase(Env.getString("RD_FORMAT", null))) {
+            belt.formatter(new JsonFormatter());
+        }
+    }
+
+    public static ToolBelt tool(final String name) {
         Client<RundeckApi> client = createClient();
         return ToolBelt.belt(name)
                        .defaultHelpCommands()
@@ -39,8 +73,7 @@ public class App {
                                new Scheduler(client),
                                new Tokens(client)
                        )
-                       .commandInput(new JewelInput())
-                       .buckle();
+                       .commandInput(new JewelInput());
     }
 
 
