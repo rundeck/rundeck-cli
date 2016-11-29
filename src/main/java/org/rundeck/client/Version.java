bb90dda196d7592bd2ebb5060c8c36416aaf3746
Alex Honor <alexhonor@yahoo.com>
2016-11-29 10:55:28 -0800

    Release version: 0.1.30

--- src/main/java/org/rundeck/client/Version.java
+++ src/main/java/org/rundeck/client/Version.java
@@ -5,7 +5,7 @@ package org.rundeck.client;
  * @since 11/28/16
  */
 public final class Version {
-    public static final String VERSION = "0.1.29";
+    public static final String VERSION = "0.1.30";
     public static final String NAME = "rundeck-cli";
 
     private Version() {
