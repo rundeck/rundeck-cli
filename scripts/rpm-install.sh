6da9120e2c485f73e8ff9d12075a82e95f61224f
Greg Schueler <greg.schueler@gmail.com>
2019-05-21 11:36:46 -0700

    Make rpm relocatable

---
+++ scripts/rpm-install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rpmfile=$(ls build/distributions/rundeck-cli-*.noarch.rpm)
+
+cp $rpmfile dockers/install/rpm/rundeck-cli-noarch.rpm
+docker build dockers/install/rpm -t rdcli-rpm-install
+docker run -it rdcli-rpm-install rd pond
