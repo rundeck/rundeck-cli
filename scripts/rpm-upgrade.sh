634ee9487b22f39fe427a468adbc4ae093aaad27
Greg Schueler <greg.schueler@gmail.com>
2019-06-26 11:21:02 -0700

    Fix #242 overwrite symlink if present, and remove on uninstall, add tests

---
+++ scripts/rpm-upgrade.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rpmfile=$(ls rd-cli-tool/build/distributions/rundeck-cli-*.noarch.rpm)
+
+cp $rpmfile dockers/install/upgrade-rpm/rundeck-cli-noarch.rpm
+docker build dockers/install/upgrade-rpm -t rdcli-rpm-upgrade
+docker run -it rdcli-rpm-upgrade rd
