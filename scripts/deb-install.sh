efc77a3837691393039f0dcd09fa604d3101e685
Greg Schueler <greg.schueler@gmail.com>
2020-03-05 16:37:46 -0800

    install tests should see output

--- scripts/deb-install.sh
+++ scripts/deb-install.sh
@@ -6,4 +6,4 @@ debfile=$(ls rd-cli-tool/build/distributions/rundeck-cli_*-1_all.deb)
 
 cp $debfile dockers/install/debian/rundeck-cli_all.deb
 docker build dockers/install/debian -t rdcli-deb
-docker run -it rdcli-deb rd pond
+docker run -it rdcli-deb rd pond | grep 'For your reference'
