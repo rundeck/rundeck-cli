a3dc285b3287e0258846258e1b5b51a11de8f2cd
Greg Schueler <greg.schueler@gmail.com>
2020-07-07 16:02:35 -0700

    Add ubuntu 18.04 install test, add deb test for rd acl is present

--- scripts/deb-install.sh
+++ scripts/deb-install.sh
@@ -5,5 +5,38 @@ set -euo pipefail
 debfile=$(ls rd-cli-tool/build/distributions/rundeck-cli_*-1_all.deb)
 
 cp $debfile dockers/install/debian/rundeck-cli_all.deb
-docker build dockers/install/debian -t rdcli-deb
-docker run -it rdcli-deb rd pond | grep 'For your reference'
+
+tag=rdcli-deb
+UBUNTUVERS="16.04 18.04"
+
+test_basic() {
+  local TAG=$1
+  docker run -it $TAG rd pond | grep 'For your reference'
+}
+test_acl() {
+  local TAG=$1
+
+  set +o pipefail
+  docker run -it $TAG rd acl | grep 'ACLPolicy'
+  set -o pipefail
+}
+build_version() {
+  local VERS=$1
+  docker build --build-arg VERS=${VERS} dockers/install/debian -t "${tag}${VERS}"
+}
+run_version() {
+  local VERS=$1
+  build_version $VERS
+
+  test_basic "${tag}${VERS}"
+  test_acl "${tag}${VERS}"
+}
+run_all() {
+  for VERS in $UBUNTUVERS; do
+    run_version $VERS
+  done
+}
+main() {
+  run_all
+}
+main
