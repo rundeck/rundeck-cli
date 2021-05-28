f8660df1c9d3e780fdadcddd72f899a71ace9b34
Greg Schueler <greg.schueler@gmail.com>
2021-05-28 11:15:31 -0700

    Add jdk11 install tests

--- scripts/install-test.sh
+++ scripts/install-test.sh
@@ -6,11 +6,13 @@ DEBTAG=rdcli-deb
 RPMTAG=rdcli-rpm
 UBUNTUVERS="16.04 18.04"
 CENTOSVERS="7 8"
+RPMJDK="java-1.8.0-openjdk java-11-openjdk"
+DEBJDK="openjdk-8-jdk openjdk-11-jdk"
 
 test_basic() {
   local TAG=$1
   set +e
-  docker run $TAG rd pond 2>&1 | grep 'For your reference'
+  docker run "$TAG" rd pond 2>&1 | grep 'For your reference'
   ret=$?
   if [ $ret -ne 0 ]; then
     echo "test_basic failed with: $ret"
@@ -22,7 +24,7 @@ test_ext() {
   local TAG=$1
   local EXT=$2
   set +e
-  docker run -e RD_DEBUG=1 $TAG rd version 2>&1 | grep "Including extension: $EXT"
+  docker run -e RD_DEBUG=1 "$TAG" rd version 2>&1 | grep "Including extension: $EXT"
   ret=$?
   if [ $ret -ne 0 ]; then
     echo "test_ext failed with: $ret"
@@ -34,19 +36,23 @@ test_ext() {
 build_deb_version() {
   local TAG=$1
   local VERS=$2
-  local debfile=$(ls rd-cli-tool/build/distributions/rundeck-cli_*-1_all.deb)
+  local JDK=$3
+  local debfile
+  debfile=$(ls rd-cli-tool/build/distributions/rundeck-cli_*-1_all.deb)
 
-  cp $debfile dockers/install/debian/rundeck-cli_all.deb
-  docker build --build-arg VERS=${VERS} dockers/install/debian -t "${TAG}"
+  cp "$debfile" dockers/install/debian/rundeck-cli_all.deb
+  docker build --build-arg VERS="${VERS}" --build-arg JDK="${JDK}" dockers/install/debian -t "${TAG}"
 }
 
 build_rpm_version() {
   local TAG=$1
   local VERS=$2
-  local rpmfile=$(ls rd-cli-tool/build/distributions/rundeck-cli-*.noarch.rpm)
+  local JDK=$3
+  local rpmfile
+  rpmfile=$(ls rd-cli-tool/build/distributions/rundeck-cli-*.noarch.rpm)
 
-  cp $rpmfile dockers/install/rpm/rundeck-cli-noarch.rpm
-  docker build --build-arg VERS=${VERS} dockers/install/rpm -t "${TAG}"
+  cp "$rpmfile" dockers/install/rpm/rundeck-cli-noarch.rpm
+  docker build --build-arg VERS="${VERS}" --build-arg JDK="${JDK}" dockers/install/rpm -t "${TAG}"
 }
 
 run_all() {
@@ -57,13 +63,17 @@ run_all() {
 }
 
 main() {
-  for VERS in $UBUNTUVERS; do
-    build_deb_version "$DEBTAG$VERS" $VERS
-    run_all "$DEBTAG$VERS"
+  for JDK in $DEBJDK; do
+    for VERS in $UBUNTUVERS; do
+      build_deb_version "$DEBTAG$VERS-$JDK" "$VERS" "$JDK"
+      run_all "$DEBTAG$VERS-$JDK"
+    done
   done
-  for VERS in $CENTOSVERS; do
-    build_rpm_version "$RPMTAG$VERS" $VERS
-    run_all "$RPMTAG$VERS"
+  for JDK in $RPMJDK; do
+    for VERS in $CENTOSVERS; do
+      build_rpm_version "$RPMTAG$VERS-$JDK" "$VERS" "$JDK"
+      run_all "$RPMTAG$VERS-$JDK"
+    done
   done
 }
 
