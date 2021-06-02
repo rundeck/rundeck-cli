d97700691f1f7b6fed16509e6b2ff7e575374d77
Greg Schueler <greg.schueler@gmail.com>
2021-06-02 11:06:05 -0700

    rpm upgrade test does not need interactive tty

--- scripts/rpm-upgrade.sh
+++ scripts/rpm-upgrade.sh
@@ -6,4 +6,4 @@ rpmfile=$(ls rd-cli-tool/build/distributions/rundeck-cli-*.noarch.rpm)
 
 cp $rpmfile dockers/install/upgrade-rpm/rundeck-cli-noarch.rpm
 docker build dockers/install/upgrade-rpm -t rdcli-rpm-upgrade
-docker run -it rdcli-rpm-upgrade rd pond | grep 'For your reference'
+docker run rdcli-rpm-upgrade rd pond | grep 'For your reference'
