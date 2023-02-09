c4e83303450ae9f4dfaa5685ad233100dd103afb
Greg Schueler <greg.schueler@gmail.com>
2023-02-09 12:09:33 -0800

    update public gpg signing key downloads

--- dockers/verify/script.sh
+++ dockers/verify/script.sh
@@ -3,6 +3,7 @@ ARGS="${@}"
 DIR=${ARGS[0]}
 
 rpm --import https://raw.githubusercontent.com/rundeck/packaging/main/pubring.gpg
+rpm --import https://docs.rundeck.com/keys/BUILD-GPG-KEY-20230105.key
 
 for i in "$DIR"/rd-cli-tool/build/distributions/*.rpm; do
     rpm -K "$i"
