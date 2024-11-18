fb33902d7c46c220c135a1928d2859d917e707dd
Greg Schueler <greg.schueler@gmail.com>
2024-11-18 09:56:35 -0800

    add tests for api version requirements, fix issue with webhook
    the webhook flag incorrectly was failing if api version <47 was used

--- rd-cli-tool/src/test/groovy/org/rundeck/client/tool/commands/projects/ArchivesSpec.groovy
+++ rd-cli-tool/src/test/groovy/org/rundeck/client/tool/commands/projects/ArchivesSpec.groovy
@@ -1,18 +1,16 @@
 package org.rundeck.client.tool.commands.projects
 
-import groovy.transform.CompileStatic
-import okhttp3.ResponseBody
+
 import org.rundeck.client.api.RundeckApi
 import org.rundeck.client.api.model.AsyncProjectImportStatus
 import org.rundeck.client.api.model.ProjectImportStatus
 import org.rundeck.client.tool.CommandOutput
+import org.rundeck.client.tool.InputError
 import org.rundeck.client.tool.RdApp
 import org.rundeck.client.tool.commands.RdToolImpl
 import org.rundeck.client.tool.options.ProjectNameOptions
-import org.rundeck.client.tool.options.ProjectRequiredNameOptions
 import org.rundeck.client.util.Client
 import org.rundeck.client.util.RdClientConfig
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 import retrofit2.mock.Calls
@@ -82,6 +80,310 @@ class ArchivesSpec extends Specification {
         0 * api._(*_)
         result == 0
     }
+
+    def "api version < 34 fails for webhooks or webhook regen auth flag"() {
+
+        def api = Mock(RundeckApi)
+
+        def retrofit = new Retrofit.Builder()
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl('http://example.com/fake/').build()
+        def out = Mock(CommandOutput)
+        def client = new Client(api, retrofit, null, null, apiversion, true, null)
+
+        def rdapp = Mock(RdApp) {
+            getClient() >> client
+            getAppConfig() >> Mock(RdClientConfig)
+        }
+        def rdTool = new RdToolImpl(rdapp)
+
+        def sut = new Archives()
+        sut.rdOutput = out
+        sut.rdTool = rdTool
+        def opts = new Archives.ArchiveImportOpts()
+        opts.components = ['test-comp'].toSet()
+        opts.componentOptions = ['test-comp.key': 'value']
+        opts.file = tempFile
+        opts.project = 'Aproj'
+        if (useIncludeFlags) {
+            opts.setIncludeFlags(new HashSet<Archives.ImportFlags>([Archives.ImportFlags.webhooks]))
+        } else {
+            opts.includeWebhooks = true
+        }
+        opts.whkRegenAuthTokens = true
+
+
+        when:
+        def result = sut.importArchive(opts)
+
+        then:
+        InputError exc = thrown()
+        exc.message.contains('Cannot use --include-webhooks or --regenerate-tokens with API < 34')
+
+        where:
+        apiversion = 33
+        useIncludeFlags << [true, false]
+    }
+
+    def "api version 34 ok for webhooks or webhook regen auth flag"() {
+
+        def api = Mock(RundeckApi)
+
+        def retrofit = new Retrofit.Builder()
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl('http://example.com/fake/').build()
+        def out = Mock(CommandOutput)
+        def client = new Client(api, retrofit, null, null, 34, true, null)
+
+        def rdapp = Mock(RdApp) {
+            getClient() >> client
+            getAppConfig() >> Mock(RdClientConfig)
+        }
+        def rdTool = new RdToolImpl(rdapp)
+
+        def sut = new Archives()
+        sut.rdOutput = out
+        sut.rdTool = rdTool
+        def opts = new Archives.ArchiveImportOpts()
+        opts.components = ['test-comp'].toSet()
+        opts.componentOptions = ['test-comp.key': 'value']
+        opts.file = tempFile
+        opts.project = 'Aproj'
+
+        if (useIncludeFlags) {
+            opts.setIncludeFlags(new HashSet<Archives.ImportFlags>([Archives.ImportFlags.webhooks]))
+        } else {
+            opts.includeWebhooks = true
+        }
+        opts.whkRegenAuthTokens = true
+
+
+        when:
+        def result = sut.importArchive(opts)
+
+        then:
+        1 * api.importProjectArchive(
+                'Aproj',
+                _,
+                _,
+                _,
+                _,
+                _,
+                true,
+                true,
+                _,
+                _,
+                _,
+                _,
+                _
+        ) >> Calls.response(new ProjectImportStatus(successful: true))
+        0 * api._(*_)
+        result == 0
+        where:
+        useIncludeFlags << [true, false]
+    }
+
+    def "api version < 38 fails for node sources flag"() {
+
+        def api = Mock(RundeckApi)
+
+        def retrofit = new Retrofit.Builder()
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl('http://example.com/fake/').build()
+        def out = Mock(CommandOutput)
+        def client = new Client(api, retrofit, null, null, apiversion, true, null)
+
+        def rdapp = Mock(RdApp) {
+            getClient() >> client
+            getAppConfig() >> Mock(RdClientConfig)
+        }
+        def rdTool = new RdToolImpl(rdapp)
+
+        def sut = new Archives()
+        sut.rdOutput = out
+        sut.rdTool = rdTool
+        def opts = new Archives.ArchiveImportOpts()
+        opts.components = ['test-comp'].toSet()
+        opts.componentOptions = ['test-comp.key': 'value']
+        opts.file = tempFile
+        opts.project = 'Aproj'
+
+        if (useIncludeFlags) {
+            opts.setIncludeFlags(new HashSet<Archives.ImportFlags>([Archives.ImportFlags.nodeSources]))
+        } else {
+            opts.includeNodeSources = true
+        }
+
+
+        when:
+        def result = sut.importArchive(opts)
+
+        then:
+        InputError exc = thrown()
+        exc.message.contains('Cannot use --include-node-sources with API < 38')
+
+        where:
+        apiversion = 37
+        useIncludeFlags << [true, false]
+    }
+
+    def "api version 38 ok for node sources flag"() {
+
+        def api = Mock(RundeckApi)
+
+        def retrofit = new Retrofit.Builder()
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl('http://example.com/fake/').build()
+        def out = Mock(CommandOutput)
+        def client = new Client(api, retrofit, null, null, 38, true, null)
+
+        def rdapp = Mock(RdApp) {
+            getClient() >> client
+            getAppConfig() >> Mock(RdClientConfig)
+        }
+        def rdTool = new RdToolImpl(rdapp)
+
+        def sut = new Archives()
+        sut.rdOutput = out
+        sut.rdTool = rdTool
+        def opts = new Archives.ArchiveImportOpts()
+        opts.components = ['test-comp'].toSet()
+        opts.componentOptions = ['test-comp.key': 'value']
+        opts.file = tempFile
+        opts.project = 'Aproj'
+
+        if (useIncludeFlags) {
+            opts.setIncludeFlags(new HashSet<Archives.ImportFlags>([Archives.ImportFlags.nodeSources]))
+        } else {
+            opts.includeNodeSources = true
+        }
+
+        when:
+        def result = sut.importArchive(opts)
+
+        then:
+        1 * api.importProjectArchive(
+                'Aproj',
+                _,
+                _,
+                _,
+                _,
+                _,
+                false,
+                false,
+                _,
+                true,
+                _,
+                _,
+                _
+        ) >> Calls.response(new ProjectImportStatus(successful: true))
+        0 * api._(*_)
+        result == 0
+        where:
+        useIncludeFlags << [true, false]
+    }
+    def "api version < 47 fails for webhook regen uuids"() {
+
+        def api = Mock(RundeckApi)
+
+        def retrofit = new Retrofit.Builder()
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl('http://example.com/fake/').build()
+        def out = Mock(CommandOutput)
+        def client = new Client(api, retrofit, null, null, apiversion, true, null)
+
+        def rdapp = Mock(RdApp) {
+            getClient() >> client
+            getAppConfig() >> Mock(RdClientConfig)
+        }
+        def rdTool = new RdToolImpl(rdapp)
+
+        def sut = new Archives()
+        sut.rdOutput = out
+        sut.rdTool = rdTool
+        def opts = new Archives.ArchiveImportOpts()
+        opts.components = ['test-comp'].toSet()
+        opts.componentOptions = ['test-comp.key': 'value']
+        opts.file = tempFile
+        opts.project = 'Aproj'
+
+        if (useIncludeFlags) {
+            opts.setIncludeFlags(new HashSet<Archives.ImportFlags>([Archives.ImportFlags.webhooks]))
+        } else {
+            opts.includeWebhooks = true
+        }
+        opts.whkRegenUuid=true
+
+
+        when:
+        def result = sut.importArchive(opts)
+
+        then:
+        InputError exc = thrown()
+        exc.message.contains('Cannot use --include-webhooks with --remove-webhooks-uuids with API < 47')
+
+        where:
+        apiversion = 46
+        useIncludeFlags << [true, false]
+    }
+
+    def "api version 47 ok for webhook regen uuids flag"() {
+
+        def api = Mock(RundeckApi)
+
+        def retrofit = new Retrofit.Builder()
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl('http://example.com/fake/').build()
+        def out = Mock(CommandOutput)
+        def client = new Client(api, retrofit, null, null, 47, true, null)
+
+        def rdapp = Mock(RdApp) {
+            getClient() >> client
+            getAppConfig() >> Mock(RdClientConfig)
+        }
+        def rdTool = new RdToolImpl(rdapp)
+
+        def sut = new Archives()
+        sut.rdOutput = out
+        sut.rdTool = rdTool
+        def opts = new Archives.ArchiveImportOpts()
+        opts.components = ['test-comp'].toSet()
+        opts.componentOptions = ['test-comp.key': 'value']
+        opts.file = tempFile
+        opts.project = 'Aproj'
+
+        if (useIncludeFlags) {
+            opts.setIncludeFlags(new HashSet<Archives.ImportFlags>([Archives.ImportFlags.webhooks]))
+        } else {
+            opts.includeWebhooks = true
+        }
+        opts.whkRegenUuid=true
+
+        when:
+        def result = sut.importArchive(opts)
+
+        then:
+        1 * api.importProjectArchive(
+                'Aproj',
+                _,
+                _,
+                _,
+                _,
+                _,
+                true,
+                _,
+                true,
+                _,
+                _,
+                _,
+                _
+        ) >> Calls.response(new ProjectImportStatus(successful: true))
+        0 * api._(*_)
+        result == 0
+        where:
+        useIncludeFlags << [true, false]
+    }
+
     def "import include via direct flags"() {
 
         def api = Mock(RundeckApi)
